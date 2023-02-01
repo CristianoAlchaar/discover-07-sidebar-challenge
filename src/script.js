d6f6d735d86a8278416007de40c910690e94c8bb
Cristiano Silva <alchaarcristiano@gmail.com>
2023-02-01 12:38:48 +0000

    Fixed some toggle issues menu  in nav items

--- src/script.js
+++ src/script.js
@@ -1,13 +1,21 @@
 const aside = document.querySelector('aside');
 const toggleMenuButton = document.querySelector('#toggle-menu-button')
 const elementsThatChangesDisplay = document.querySelectorAll(".display");
-
+const itemsListThatChange = document.querySelectorAll("aside nav ul li");
+const iconContainersInsideNavUlLi = document.querySelectorAll("aside nav ul li i.icon-container");
+console.log(iconContainersInsideNavUlLi)
 function handleToggleMenuButtonClick(){
     aside.classList.toggle('expanded-aside')
     toggleMenuButton.classList.toggle('new-margin-toggle-button')
     elementsThatChangesDisplay.forEach(function(element){
         element.classList.toggle('display')
     })
+    itemsListThatChange.forEach(function(item){
+        item.classList.toggle('nav-item-expanded')
+    })
+    iconContainersInsideNavUlLi.forEach(function(container) {
+        container.classList.toggle('modified-when-expanded-icon-container-hover-bg-color');
+    });
 }
 
 toggleMenuButton.addEventListener('click', handleToggleMenuButtonClick)
